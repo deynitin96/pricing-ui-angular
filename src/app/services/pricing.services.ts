99b425b2eda42f12964c4ba8cdc17da606655bb6
deynitin96 <dey.nitin96@gmail.com>
2026-09-03 19:47:16 +0530

    Fixed Service class to call Fleet Service

--- src/app/services/pricing.services.ts
+++ src/app/services/pricing.services.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ApiResponse, PricingRequest, PricingResponse, Truck } from '../models/pricing.model';
+import {
+  ApiResponse,
+  PricingRequest,
+  PricingResponse,
+  Truck
+} from '../models/pricing.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,12 +14,15 @@ export class PricingService {
 
   private http = inject(HttpClient);
 
-  private readonly pricingApiUrl = '/pricing-api/api/v1/pricing';
-  private readonly truckApiUrl = '/truck-api/api/v1/trucks';
+  private readonly pricingApiUrl =
+    '/fleet-api/api/v1/quotes';
+
+  private readonly truckApiUrl =
+    '/truck-api/api/v1/trucks';
 
   calculateQuote(request: PricingRequest) {
     return this.http.post<ApiResponse<PricingResponse>>(
-      `${this.pricingApiUrl}/quote`,
+      `${this.pricingApiUrl}/calculate`,
       request
     );
   }
